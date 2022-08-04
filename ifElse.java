7c2d86a4936f9a6c7678428b627289405efce1f7
Vaishnavi Waghmale <102860344+Vaishnavi-Waghmale@users.noreply.github.com>
2022-08-04 18:51:25 +0530

    Add files via upload
    New problem

---
+++ ifElse.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class ifElse {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        if (a > 15) {
+            System.out.println("Adult");
+        } else {
+            System.out.println("Kid");
+        }
+    }
+}
