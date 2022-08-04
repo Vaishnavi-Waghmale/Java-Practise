4f95a01e437ddc91320ef7d040603289cce0a288
Vaishnavi Waghmale <102860344+Vaishnavi-Waghmale@users.noreply.github.com>
2022-08-04 18:53:35 +0530

    Add files via upload
    New problem

---
+++ variables.java
@@ -0,0 +1,10 @@
+public class variables {
+    public static void main(String args[]) {
+        String name = "Vaishnavi";
+        int a = 25;
+        int b = 12;
+        int c = a + b;
+        System.out.println(name);
+        System.out.print(c);
+    }
+}
