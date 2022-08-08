b1d765ffcae851668b7dcd92d7e4423aa6367c99
Vaishnavi Waghmale <102860344+Vaishnavi-Waghmale@users.noreply.github.com>
2022-08-08 16:51:52 +0530

    Add files via upload
    New problem

---
+++ Solid_Rectangle.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Solid_Rectangle {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
