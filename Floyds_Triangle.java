2fccb571ffd0259ad0abc01cfb3bdcc24f58ec80
Vaishnavi Waghmale <102860344+Vaishnavi-Waghmale@users.noreply.github.com>
2022-08-08 16:49:11 +0530

    Add files via upload
    New problem

---
+++ Floyds_Triangle.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Floyds_Triangle {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int number = 1;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(number + " ");
+                number++;
+            }
+            System.out.println();
+        }
+    }
+}
