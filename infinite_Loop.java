c7072fbc8395327f76504471ff5c7c9ee9cd930c
Vaishnavi Waghmale <102860344+Vaishnavi-Waghmale@users.noreply.github.com>
2022-08-11 15:00:05 +0530

    Add files via upload
    New problem

---
+++ infinite_Loop.java
@@ -0,0 +1,10 @@
+public class infinite_Loop {
+    public static void main(String args[]) {
+        int x = 10;
+        do {
+            System.out.println("Hello");
+            x++;
+        } while (x > 10);
+
+    }
+}
