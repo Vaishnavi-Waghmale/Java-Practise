296431068df371459a386177cd5f3642c0d47183
Vaishnavi Waghmale <102860344+Vaishnavi-Waghmale@users.noreply.github.com>
2022-08-15 10:36:34 +0530

    Add files via upload
    New problem

---
+++ power.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class power {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int n = sc.nextInt();
+        int ans = 1;
+        for (int i = 1; i <= n; i++) {
+            ans *= x;
+        }
+        System.out.println(ans);
+
+    }
+}
