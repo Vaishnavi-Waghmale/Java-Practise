7fe78812e3289ba63eac832e4116191968a2fa92
Vaishnavi Waghmale <102860344+Vaishnavi-Waghmale@users.noreply.github.com>
2022-08-15 12:26:28 +0530

    New problem

---
+++ SearchX.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class SearchX {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int rows = sc.nextInt();
+        int cols = sc.nextInt();
+
+        int[][] numbers = new int[rows][cols];
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                numbers[i][j] = sc.nextInt();
+            }
+        }
+        int a = sc.nextInt();
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (numbers[i][j] == a) {
+                    System.out.println("Number is fount at locations (" + i + ',' + j + ")");
+                }
+            }
+        }
+
+    }
+}
