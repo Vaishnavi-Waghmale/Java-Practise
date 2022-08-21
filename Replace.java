dfcec0aabbb17336208e31a7b2d65f81b1d83878
Vaishnavi Waghmale <102860344+Vaishnavi-Waghmale@users.noreply.github.com>
2022-08-21 14:25:15 +0530

    New problem

---
+++ Replace.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Replace {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        String result = "";
+
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == 'e') {
+                result += 'i';
+            } else {
+                result += str.charAt(i);
+            }
+        }
+        System.out.println(result);
+    }
+}
